f34932aaac3f7a92ffc3e331f0feab0c0f06fd69
JBV777 <34755273+JBV777@users.noreply.github.com>
2019-05-27 17:07:34 -0300

    Adding a javascript file that does the calculation

---
+++ gscript.js
@@ -0,0 +1,18 @@
+var coursenum=0.0;
+var totalgrade=0.0;
+
+document.getElementById("calculate").onclick=function(){calculation();};
+
+
+function calculation(){
+	var ind=document.getElementById("grade").value;	
+	var individualgrade=Number(ind);
+    coursenum=coursenum+1.0;
+    totalgrade=totalgrade+individualgrade;
+    var aver=totalgrade/coursenum;
+    document.getElementById("num").style.display="block";
+    document.getElementById("course").innerHTML=coursenum;
+
+    document.getElementById("average").style.display="block";
+    document.getElementById("av").innerHTML=aver;
+}
